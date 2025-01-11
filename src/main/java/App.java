d6a3a5a6ed85c4ca99cca5e94ef82696526c2a5e
DenKu <Denkuzia5155@gmail.com>
2025-01-11 16:15:32 +0300

    #1 First Commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,8 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.sql.SQLOutput;
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
@@ -8,5 +10,21 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat cat1 =
+                (Cat) applicationContext.getBean("Cat");
+        System.out.println(cat1.getName());
+
+        Cat cat2 =
+                (Cat) applicationContext.getBean("Cat");
+        System.out.println(cat2.getName());
+
+        System.out.println(bean == bean2);
+        System.out.println(cat1 == cat2);
+
     }
 }
